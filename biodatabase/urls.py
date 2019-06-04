105762e40b4e937e0e3e02e063b0073917a04b8c
Crazybert <botengyao@outlook.com>
2019-06-04 16:38:30 +0800

    multi display

--- biodatabase/urls.py
+++ biodatabase/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.genebook, name='genebook'),
     path('test/', views.ajax_dict, name='ajax-dict'),
     path('test2/', views.ajax_list, name='ajax-list'),
-    path('search/', views.search, name='search')
+    path('search/', views.search, name='search'),
+    path('searchm/', views.search_multi, name='searchm')
     #path('<int:blog_id>/', views.blogdetails, name='blogdetails' )
 ]
--- biodatabase/views.py
+++ biodatabase/views.py
@@ -17,6 +17,7 @@ def genebook(request):
                   {'gene_dbs':gene_db,'info':info})
 def search(request):
     gene_db = GeneDbLink.objects
+    infor =[]
     try:
         gene_pool = request.GET['genepool']
         search_id = request.GET['gene_id']
@@ -41,7 +42,63 @@ def search(request):
     # summary_info2 = list(summary2)
     #summary_info = list(summary)
     return render(request, 'biodatabase/templates/genebooks.html',\
-                  {'gene_dbs':gene_db,'info':info})
+                  {'gene_dbs':gene_db,'info':info,'infor':infor})
+def search_multi(request):
+    gene_db = GeneDbLink.objects
+    infor = []
+
+    gene_pool = request.GET['genepool']
+    try:
+        search_id = request.GET['gene_id'].split()
+    except:
+        render(request, 'biodatabase/templates/genebooksmulti.html', \
+               {'gene_dbs': gene_db, 'infor': infor})
+    if gene_pool == "Gene Name":
+        for id in search_id:
+            try:
+                infor.append(GeneDbLink.objects.get(name=id))
+            except:
+                continue
+    elif gene_pool == "HGNC":
+        for id in search_id:
+            try:
+                infor.append(GeneDbLink.objects.get(hgnc=id))
+            except:
+                continue
+    elif gene_pool == "ENTREZ":
+        for id in search_id:
+            try:
+                infor.append(GeneDbLink.objects.get(entrez_gene=id))
+            except:
+                continue
+    elif gene_pool == "ENSEMBL":
+        for id in search_id:
+            try:
+                infor.append(GeneDbLink.objects.get(ensembl=id))
+            except:
+                continue
+    elif gene_pool == "UNIPROTKB":
+        for id in search_id:
+            try:
+                infor.append(GeneDbLink.objects.get(uniprotkb=id))
+            except:
+                continue
+    elif gene_pool == "OMIM":
+        for id in search_id:
+            try:
+                infor.append(GeneDbLink.objects.get(omim=id))
+            except:
+                continue
+    #except:
+      #  return render(request, 'biodatabase/templates/genebooksmulti.html')
+    # info2 = GenecardDescriptionSummary.objects.get(gene__name="KLK13")
+    # filter(子表外键字段__母表字段='过滤条件')
+    # summary2 = GenecardDescriptionSummary.objects.filter(gene__name = 'KLK13').values('gene__name','type','description','summary_entrez','summary_genecard')
+    # summary_info2 = list(summary2)
+    #summary_info = list(summary)
+    return render(request, 'biodatabase/templates/genebooksmulti.html',\
+                  {'gene_dbs':gene_db, 'infor':infor})
+
 def ajax_list(request):
     a = [1,2,3,4,5,6,7,8,9,10]
     return JsonResponse(a, safe=False)
@@ -49,6 +106,19 @@ def ajax_list(request):
 def ajax_dict(request):
     name_dict = {'twz': 'Love python and Django', 'zqxt': 'I am teaching Django'}
     return JsonResponse(name_dict)
+
+def index(request):
+    response = HttpResponse(content_type='text/csv')
+    # attachment 代表这个csv文件作为一个附件的形式下载
+    # filename='abc.csv' 指定下载的文件名字
+    response['Content-Disposition'] = "attachment;filename='abc.csv'"
+    writer = csv.writer(response)
+    writer.writerow(['username','age','height', 'weight'])
+    writer.writerow(['zhiliao','18','180','100'])    # 在这里并不会指定文件名字。
+
+    return response
+
+
 '''
 def add(request):
     a = request.GET['a']
