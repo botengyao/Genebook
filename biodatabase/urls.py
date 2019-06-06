49fc74d2008cd498ac3b4aa2807cbc51af24a1db
Crazybert <botengyao@outlook.com>
2019-06-06 15:32:46 +0800

    download function

--- biodatabase/urls.py
+++ biodatabase/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
-
 from biodatabase import views
+from django.http import HttpResponse
 
 urlpatterns = [
     path('', views.genebook, name='genebook'),
-    path('test/', views.ajax_dict, name='ajax-dict'),
-    path('test2/', views.ajax_list, name='ajax-list'),
     path('search/', views.search, name='search'),
-    path('searchm/', views.search_multi, name='searchm')
+    path('searchmulti/', views.search_multi, name='searchm'),
+    path('search/downloads', views.downloads, name='downloads'),
+    path('search/download', views.download, name='download')
     #path('<int:blog_id>/', views.blogdetails, name='blogdetails' )
 ]
--- biodatabase/views.py
+++ biodatabase/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from .models import GeneDbLink
 from django.http import JsonResponse
 from django.http import HttpResponse
+#from django.views import View
 import json
+import csv
 # Create your views here.
+infor = []
+
 def genebook(request):
     gene_db = GeneDbLink.objects
     info = GeneDbLink.objects.get(omim='611000')
@@ -18,6 +22,8 @@ def genebook(request):
 def search(request):
     gene_db = GeneDbLink.objects
     infor =[]
+    global info
+    info = GeneDbLink.objects.get(omim='611000')
     try:
         gene_pool = request.GET['genepool']
         search_id = request.GET['gene_id']
@@ -34,7 +40,7 @@ def search(request):
         elif gene_pool == "OMIM":
             info = GeneDbLink.objects.get(omim=search_id)
     except:
-        return render(request, 'biodatabase/templates/genebooks.html')
+        return render(request, 'biodatabase/templates/genebooks.html',{'info':info})
 
     # info2 = GenecardDescriptionSummary.objects.get(gene__name="KLK13")
     # filter(子表外键字段__母表字段='过滤条件')
@@ -45,6 +51,7 @@ def search(request):
                   {'gene_dbs':gene_db,'info':info,'infor':infor})
 def search_multi(request):
     gene_db = GeneDbLink.objects
+    global infor
     infor = []
 
     gene_pool = request.GET['genepool']
@@ -99,26 +106,31 @@ def search_multi(request):
     return render(request, 'biodatabase/templates/genebooksmulti.html',\
                   {'gene_dbs':gene_db, 'infor':infor})
 
-def ajax_list(request):
-    a = [1,2,3,4,5,6,7,8,9,10]
-    return JsonResponse(a, safe=False)
-
-def ajax_dict(request):
-    name_dict = {'twz': 'Love python and Django', 'zqxt': 'I am teaching Django'}
-    return JsonResponse(name_dict)
-
-def index(request):
+def downloads(request):
     response = HttpResponse(content_type='text/csv')
     # attachment 代表这个csv文件作为一个附件的形式下载
     # filename='abc.csv' 指定下载的文件名字
-    response['Content-Disposition'] = "attachment;filename='abc.csv'"
+    response['Content-Disposition'] = "attachment; filename= muti_convert_summary.csv "
     writer = csv.writer(response)
-    writer.writerow(['username','age','height', 'weight'])
-    writer.writerow(['zhiliao','18','180','100'])    # 在这里并不会指定文件名字。
-
+    writer.writerow(['name', 'HGNC','Entrez_Gene', 'Ensembl', 'UniProtKB', 'OMIM', 'type', 'description', \
+                     'summary_entrez', 'summary_genecard', 'summary_uniport', 'summary_Tocris', 'summary_CIViC'])
+    for info in infor:
+        writer.writerow([info.name, info.hgnc, info.entrez_gene, info.ensembl, info.uniprotkb, info.omim, \
+                         info.type, info.description, info.summary_entrez, info.summary_genecard,\
+                         info.summary_uniport, info.summary_tocris, info.summary_civic])
     return response
-
-
+def download(request):
+    response = HttpResponse(content_type='text/csv')
+    # attachment 代表这个csv文件作为一个附件的形式下载
+    # filename='abc.csv' 指定下载的文件名字
+    response['Content-Disposition'] = "attachment; filename= single_convert_summary.csv "
+    writer = csv.writer(response)
+    writer.writerow(['name', 'HGNC','Entrez_Gene', 'Ensembl', 'UniProtKB', 'OMIM', 'type', 'description', \
+                     'summary_entrez', 'summary_genecard', 'summary_uniport', 'summary_Tocris', 'summary_CIViC'])
+    writer.writerow([info.name, info.hgnc, info.entrez_gene, info.ensembl, info.uniprotkb, info.omim, \
+                         info.type, info.description, info.summary_entrez, info.summary_genecard,\
+                         info.summary_uniport, info.summary_tocris, info.summary_civic])
+    return response
 '''
 def add(request):
     a = request.GET['a']
@@ -126,4 +138,14 @@ def add(request):
     a = int(a)
     b = int(b)
     return HttpResponse(str(a+b))
+'''
+'''
+
+def ajax_list(request):
+    a = [1,2,3,4,5,6,7,8,9,10]
+    return JsonResponse(a, safe=False)
+
+def ajax_dict(request):
+    name_dict = {'twz': 'Love python and Django', 'zqxt': 'I am teaching Django'}
+    return JsonResponse(name_dict)
 '''
