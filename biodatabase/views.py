c94ec35a0a72ba30bd84ab480a93982efed1142d
Crazybert <botengyao@outlook.com>
2019-06-11 15:19:01 +0800

    1.1 search strategy

--- biodatabase/views.py
+++ biodatabase/views.py
@@ -3,6 +3,7 @@ from .models import GeneDbLink
 from django.http import JsonResponse
 from django.http import HttpResponse
 #from django.views import View
+from django.db.models import Q
 import json
 import csv
 # Create your views here.
@@ -22,25 +23,43 @@ def genebook(request):
 def search(request):
     gene_db = GeneDbLink.objects
     infor =[]
-    global info
-    info = GeneDbLink.objects.get(omim='611000')
+    global info_filter
+    info_filter = GeneDbLink.objects.filter(name="")
+
     try:
         gene_pool = request.GET['genepool']
         search_id = request.GET['gene_id']
-        if gene_pool == "Gene Name":
-            info = GeneDbLink.objects.get(name=search_id)
-        elif gene_pool == "HGNC":
-            info = GeneDbLink.objects.get(hgnc=search_id)
-        elif gene_pool == "ENTREZ":
-            info = GeneDbLink.objects.get(entrez_gene=search_id)
-        elif gene_pool == "ENSEMBL":
-            info = GeneDbLink.objects.get(ensembl=search_id)
-        elif gene_pool == "UNIPROTKB":
-            info = GeneDbLink.objects.get(uniprotkb=search_id)
-        elif gene_pool == "OMIM":
-            info = GeneDbLink.objects.get(omim=search_id)
+        if search_id:
+            if gene_pool == "Fuzzy Search":
+                info_filter = GeneDbLink.objects.filter(Q(name=search_id)|Q(hgnc=search_id)|Q(entrez_gene=search_id)|Q(ensembl=search_id)\
+                                                        |Q(uniprotkb=search_id)|Q(omim=search_id))
+                if not info_filter:
+                    info_filter = GeneDbLink.objects.filter(Q(name__regex='^'+ search_id + '\d') | Q(hgnc__regex='^'+ search_id + '\d') \
+                                                            | Q(entrez_gene__regex='^'+ search_id + '\d') | Q(ensembl__regex='^'+ search_id + '\d') \
+                                                            | Q(uniprotkb__regex='^'+ search_id + '\d') | Q(omim__regex='^'+ search_id + '\d'))
+                    if not info_filter:
+                        info_filter = GeneDbLink.objects.filter(description__icontains = search_id)
+                        if len(info_filter) >= 35:
+                            info_filter = info_filter[:36]
+
+            elif gene_pool == "Gene Name":
+                #info = GeneDbLink.objects.get(name = search_id)
+                info_filter = GeneDbLink.objects.filter(name__regex='^'+ search_id)
+            elif gene_pool == "HGNC":
+                info_filter = GeneDbLink.objects.filter(hgnc=search_id)
+            elif gene_pool == "ENTREZ":
+                info_filter = GeneDbLink.objects.filter(entrez_gene=search_id)
+            elif gene_pool == "ENSEMBL":
+                info_filter = GeneDbLink.objects.filter(ensembl=search_id)
+            elif gene_pool == "UNIPROTKB":
+                info_filter = GeneDbLink.objects.filter(uniprotkb=search_id)
+            elif gene_pool == "OMIM":
+                info_filter = GeneDbLink.objects.filter(omim=search_id)
+        else:
+            info_filter = GeneDbLink.objects.filter(name="")
+
     except:
-        return render(request, 'biodatabase/templates/genebooks.html',{'info':info})
+        return render(request, 'biodatabase/templates/genebooks.html',{'info_filter':info_filter})
 
     # info2 = GenecardDescriptionSummary.objects.get(gene__name="KLK13")
     # filter(子表外键字段__母表字段='过滤条件')
@@ -48,7 +67,9 @@ def search(request):
     # summary_info2 = list(summary2)
     #summary_info = list(summary)
     return render(request, 'biodatabase/templates/genebooks.html',\
-                  {'gene_dbs':gene_db,'info':info,'infor':infor})
+                  {'gene_dbs':gene_db,'infor':infor,'info_filter':info_filter, "search_id":search_id,"pool":json.dumps(gene_pool)})
+
+
 def search_multi(request):
     gene_db = GeneDbLink.objects
     global infor
@@ -96,6 +117,7 @@ def search_multi(request):
                 infor.append(GeneDbLink.objects.get(omim=id))
             except:
                 continue
+
     #except:
       #  return render(request, 'biodatabase/templates/genebooksmulti.html')
     # info2 = GenecardDescriptionSummary.objects.get(gene__name="KLK13")
@@ -127,7 +149,8 @@ def download(request):
     writer = csv.writer(response)
     writer.writerow(['name', 'HGNC','Entrez_Gene', 'Ensembl', 'UniProtKB', 'OMIM', 'type', 'description', \
                      'summary_entrez', 'summary_genecard', 'summary_uniport', 'summary_Tocris', 'summary_CIViC'])
-    writer.writerow([info.name, info.hgnc, info.entrez_gene, info.ensembl, info.uniprotkb, info.omim, \
+    for info in info_filter:
+        writer.writerow([info.name, info.hgnc, info.entrez_gene, info.ensembl, info.uniprotkb, info.omim, \
                          info.type, info.description, info.summary_entrez, info.summary_genecard,\
                          info.summary_uniport, info.summary_tocris, info.summary_civic])
     return response
